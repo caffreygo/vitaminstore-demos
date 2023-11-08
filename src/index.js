4f5a6f6fb909729b7515f2e78fd5575ce20032d8
Jinrui <jinrui@kooboo.cn>
2023-11-08 17:01:46 +0800

    final

--- src/index.js
+++ src/index.js
@@ -1,77 +1,75 @@
-async function unsubscribeAll() {
-  let result = await submitToVitaminstore();
-  if (result) {
-    document.getElementById("form").submit();
-  }
-}
+function submitToVitaminstore(callback) {
+  let formData;
 
-async function changeSubscription() {
-  let result = await submitToVitaminstore();
-  if (result) {
-    document.getElementById("form").submit();
-  }
-}
-
-async function submitToVitaminstore() {
   try {
-    let email = document.getElementById("email").innerText;
-    let newsletter = document.getElementById("category_newsletter").checked;
-    let promotional = document.getElementById(
-      "category_promotional-email"
-    ).checked;
-    let physicalMagazine = document.getElementById(
-      "category_physical-magazine"
-    ).checked;
-    let model = {
-      Email: email,
-      PhysicalMagazineSubscribed: newsletter,
-      NewsletterSubscribed: promotional,
-      PromotionalEmailSubscribed: physicalMagazine,
+    const formEl = document.getElementById("form");
+
+    formData = {
+      Email: document.getElementById("email").innerText,
+      NewsletterSubscribed: formEl.querySelector('input[name="newsletter"]')
+        .checked,
+      PromotionalEmailSubscribed: formEl.querySelector(
+        'input[name="promotional-email"]'
+      ).checked,
+      PhysicalMagazineSubscribed: formEl.querySelector(
+        'input[name="physical-magazine"]'
+      ).checked,
     };
-    const response = await fetch(
-      "https://localhost:5009/bloomreach/update-consent",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: "Apikey 1ad5f3ac-d91f-432f-90f1-58f1fd11aef4",
-        },
-        body: JSON.stringify(model),
-        mode: "cors",
-        credentials: "same-origin",
-        cache: "no-cache",
-      }
+  } catch (error) {
+    console.error(error);
+    callback(
+      false,
+      "Veldnamen komen niet overeen, controleer op wijzigingen."
+    );
+    return;
+  }
+
+  try {
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+      "POST",
+      "https://marketing-api-dev.vitaminstore.nl/bloomreach/update-consent",
+      true
+    );
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader(
+      "Authorization",
+      "Apikey 1ad5f3ac-d91f-432f-90f1-58f1fd11aef4"
     );
 
-    if (response.ok) {
-      return true;
-    } else {
-      //alert('encounter some error please try again later');
-      return false;
-    }
-  } catch (ex) {
-    console.log(ex);
-    //alert('encounter network issue  please try again later');
+    xhr.withCredentials = true;
+    xhr.setRequestHeader("Cache-Control", "no-cache");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          callback(true);
+        } else if (xhr.status === 500) {
+          callback(false, "Interne serverfout, probeer het later opnieuw.");
+        } else {
+          callback(false, "Onbekende fout opgetreden.");
+        }
+      }
+    };
+
+    xhr.send(JSON.stringify(formData));
+  } catch (error) {
+    console.error(error);
+    callback(
+      false,
+      "Kan geen verbinding maken met de server, controleer alstublieft de internetverbinding."
+    );
   }
 }
 
-// // 获取表单和提交按钮
-// const form = document.getElementById("subscription-form");
-// const submitButton = document.getElementById("submit-button");
-
-// // 添加点击事件监听器
-// submitButton.addEventListener("click", function () {
-//     // 获取选中的复选框的值
-//     const formData = {
-//         newsletter: form.querySelector('input[name="newsletter"]').checked,
-//         promotionalEmail: form.querySelector('input[name="promotionalEmail"]').checked,
-//         physicalMagazine: form.querySelector('input[name="physicalMagazine"]').checked,
-//     };
-
-//     // 这里可以添加额外的逻辑，如将数据发送到服务器
-//     console.log(formData);
-
-//     // 如果要提交表单，可以使用以下代码：
-//     // form.submit();
-// });
+function changeSubscription() {
+  submitToVitaminstore(function (success, errorMessage) {
+    if (success) {
+      // document.getElementById('form').submit();
+    } else {
+      document.getElementById("errorAlert").style.display = "block";
+      document.getElementById("errorAlertContent").innerText = errorMessage;
+    }
+  });
+}
